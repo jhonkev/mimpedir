22afe29a22184060f5c23e44d8eb2e6339043ea0
Aluno 05 <jhonkevinsiqueira@gmail.com>
2025-10-06 17:20:44 -0300

    restaurante dao e tela home

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:mimpedir/banco/restaurante_DAO.dart';
 
-class TelaHome extends StatelessWidget {
+class TelaHome extends StatefulWidget {
   TelaHome({super.key});
+
+  @override
+  State<TelaHome> createState() => TelaHomeState();
+}
+  class TelaHomeState extends State<TelaHome>{
+  List<Restaurante> restaurante = [];
+
+
+  Future<void> carregarRestaurantes() async {
+    final lista = await RestauranteDAO.listarTodos();
+    setState(() {
+      restaurantes = lista;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
   return Scaffold(
-    appBar: AppBar(title: const Text('Tela Home')),
-    body: Center(
-      child: Text('Seja bem vindo ao Mim Pedir!!!'),
+    appBar: AppBar(
+        title: const Text('Lista de Restaurantes'),
+      actions: [
+        IconButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
+      },
+      icon: Icon(Icons.add)
+        )
+      ],
     ),
+    body: Padding(padding:  const EdgeInsets.all(10),
+      child: ListView.builder(
+          itemCount: restaurantes.lenght,
+          itemBuilder: (context, index){
+            final r = restaurantes[index];
+            return Card(
+              margin: EdgeInsets.symmetric(vertical: 8),
+              child: ListTile(
+                title: Text(r.nome ?? 'sem nome'),
+                subtitle: Text('ID: ${r.codigo}'),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
+                    }, icon: Icon(Icons.edit, color: Colors.blue,)),
+                    IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red)),
+                  ],
+                )
+              ),
+            );
+          }
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+    },
+    child: Icon(Icons.add)
+  ),
+bottomNavigationBar: BottomNavigationBar(
+    items: const<BottomNavigationBarItem>[
+      BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Adicionar'),
+      BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Adicionar'),
+  ],
+  ),
+
   );
   }
 }
